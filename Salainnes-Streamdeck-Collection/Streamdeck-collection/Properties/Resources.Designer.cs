35c68244c479a65db5efc2ad58ddb64bf173f621
Salainne <21008184+Salainne@users.noreply.github.com>
2025-05-18 12:02:21 +0200

    Add ImageTransparencyTest action and transparency resource
    This commit doth introduce a new resource, `transparency`,
    within the `Resources.Designer.cs`, fetching a localized
    bitmap image now linked to `transparency.png`. A new
    source file, `ImageTransparencyTest.cs`, is added,
    defining a static class for a Stream Deck action that
    sets an image and title using the `transparency` resource.
    The project file, `Streamdeck-collection.csproj`, is
    updated to include this new source. Furthermore,
    `manifest.json` is modified to encompass the new action,
    detailing its icon, name, tooltip, and UUID. Lastly,
    the binary data for the `transparency.png` image,
    crafted with Paint.NET, is now included.

--- Salainnes-Streamdeck-Collection/Streamdeck-collection/Properties/Resources.Designer.cs
+++ Salainnes-Streamdeck-Collection/Streamdeck-collection/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace Streamdeck_collection.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap transparency {
+            get {
+                object obj = ResourceManager.GetObject("transparency", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
---
+++ Salainnes-Streamdeck-Collection/Streamdeck-collection/StreamdeckActions/ImageTransparencyTest.cs
@@ -0,0 +1,39 @@
+﻿using streamdeck_client_csharp;
+using streamdeck_client_csharp.Events;
+using Streamdeck_collection.Model;
+using Streamdeck_collection.Model.Attributes;
+using Streamdeck_collection.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Streamdeck_collection.StreamdeckActions
+{
+    [StreamdeckAction("com.salainne.salainnes-streamdeck-collection.ImageTransparencyTest")]
+    public static class ImageTransparencyTest
+    {
+        public static void Run(StreamDeckConnection connection, KeyValuePair<string, ActiveIconContainer> icon)
+        {
+            if (icon.Value.Data == null)
+            {
+                icon.Value.Data = new ImageTestData() { UpdateInterval = 10000 };
+                _ = connection.SetImageAsync("", icon.Key, SDKTarget.HardwareAndSoftware, null);
+                _ = connection.SetTitleAsync("", icon.Key, SDKTarget.HardwareAndSoftware, null);
+            }
+            
+
+            var tmp = DateTime.Now.ToString("HH:mm:ss");
+            _ = connection.SetImageAsync(Helpers.Utilities.GetBase64Image(Resources.transparency), icon.Key, SDKTarget.HardwareAndSoftware, null);
+            //_ = connection.SetImageAsync(_img, icon.Key, SDKTarget.HardwareAndSoftware, null);
+            _ = connection.SetTitleAsync(tmp, icon.Key, SDKTarget.HardwareAndSoftware, null);
+
+        }
+    }
+}
